9879f4e9cbce927294e47648e1c86567ac192791
MironovDA-1971 <mironovda@gmail.com>
2022-11-17 00:23:33 +0300

    Stage 2/6: Expand the search READY
    Stage 2/6: Expand the search
    Ready

--- Main.kt
+++ Main.kt
@@ -1,7 +1,30 @@
 package search
 
 fun main() {
-    val stringList = readln().split(" ")
-    val word = readln()
-    println(if (stringList.contains(word)) stringList.indexOf(word) + 1 else "Not found")
+    //val stringList = readln().split(" ")
+    //val word = readln()
+    //println(if (stringList.contains(word)) stringList.indexOf(word) + 1 else "Not found")
+    val message = PrintMessage()
+    println(message.enterNumberOfPeople)
+    val numOfPeople = readln().toInt()
+    println(message.enterAllPeople)
+    val allPeopleList = List(numOfPeople) { readln().trim() }
+    println(message.enterNumberOfQueries)
+    val numQueries = readln().toInt()
+    for (a in 1..numQueries) {
+        println(message.enterDataToSearchPeople)
+        val searchKey = readln().trim().lowercase()
+        var flag = 0
+        for (i in allPeopleList.indices) {
+            if (allPeopleList[i].lowercase().contains(searchKey)) {
+                if (flag == 0) {
+                    println(message.peopleFound)
+                    flag = 1
+                }
+                println(allPeopleList[i])
+            }
+        }
+        if (flag == 0) println("${message.noMatchingPeopleFound}")
+        //else println("\n")
+    }
 }
---
+++ classes.kt
@@ -0,0 +1,10 @@
+package search
+
+data class PrintMessage(var maxNumber: Int = 0) {
+    val enterNumberOfPeople = "Enter the number of people:"
+    val enterAllPeople = "Enter all people:"
+    val enterNumberOfQueries = "\nEnter the number of search queries:"
+    val enterDataToSearchPeople = "\nEnter data to search people:"
+    val peopleFound = "People found:"
+    val noMatchingPeopleFound = "No matching people found."
+}
