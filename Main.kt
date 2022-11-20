39c474965b1d63edd04312a434bcb4cea5d55565
MironovDA-1971 <mironovda@gmail.com>
2022-11-21 00:51:38 +0300

    Stage 5/6: Inverted Index search

--- Main.kt
+++ Main.kt
@@ -3,14 +3,16 @@ package search
 fun main(args: Array<String>) {
 
     val fileName = args[args.indexOf("--data") + 1]
-    val allPeopleList = readFile(fileName).split("\n")
+    val allPeopleList = readFile(fileName).split(System.lineSeparator())
+    val mapIndex = getIndexKey(allPeopleList)
+    //mapIndex.forEach { println(it) }
 
     val message = PrintMessage()
     val userMenu = UserMenu()
     while (true) {
         when(userMenu.outMenu()) {
             0 -> break
-            1 -> searchQuery(allPeopleList, message)
+            1 -> indexSearch(mapIndex, allPeopleList, message) //searchQuery(allPeopleList, message)
             2 -> printPerson(allPeopleList, message)
             else -> message.menuInputError
         }
--- classes.kt
+++ classes.kt
@@ -1,6 +1,7 @@
 package search
 
 import java.io.File
+import java.util.NoSuchElementException
 
 data class PrintMessage(var maxNumber: Int = 0) {
     //val enterNumberOfPeople = "Enter the number of people:"
@@ -8,6 +9,7 @@ data class PrintMessage(var maxNumber: Int = 0) {
 
     val enterDataToSearchPeople = "\nEnter a name or email to search all suitable people."
     val peopleFound = "People found:"
+    val personFound = "person found:"
     val noMatchingPeopleFound = "No matching people found."
     val listOfPeople = "\n=== List of people ==="
 
@@ -36,6 +38,32 @@ class UserMenu {
         val num = readln().toInt()
         return if (num in range) num
         else -1
+    }
+}
+
+fun getIndexKey(listPerson: List<String>): MutableMap<String, MutableList<Int>> {
+    val indexMap = emptyMap<String, MutableList<Int>>().toMutableMap()
+
+    for (i in listPerson.indices) {
+        val tmp = listPerson[i].lowercase().split(" ")
+        for (a in tmp.indices) {
+            if (indexMap.putIfAbsent(tmp[a], mutableListOf(i)) != null) {
+                indexMap[tmp[a]]?.add(i)
+            } else indexMap[tmp[a]] = MutableList(1){i}
+        }
+    }
+    return indexMap
+}
+
+fun indexSearch(indexMap: MutableMap<String, MutableList<Int>>, listPerson: List<String>, message: PrintMessage) {
+    println(message.enterDataToSearchPeople)
+    val searchKey = readln().trim().lowercase()
+    try {
+        val indexNum = indexMap.filterKeys { it.lowercase() == searchKey }.getValue(searchKey)
+        println("${indexNum.size} ${message.personFound}")
+        for (i in indexNum.indices) println(listPerson[indexNum[i]])
+    } catch (e: NoSuchElementException) {
+        println(message.noMatchingPeopleFound)
     }
 }
 
